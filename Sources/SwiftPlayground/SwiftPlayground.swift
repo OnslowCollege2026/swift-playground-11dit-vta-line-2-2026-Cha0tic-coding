9a49ba5d93bb119e1942005511b1ad9115450946
oc-Riley-Evans-26 <riley.evans@student.onslow.school.nz>
2026-04-23 02:08:15 +0000

    added some words

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -4,6 +4,18 @@
 @main
 struct SwiftPlayground {
     static func main() {
+        let FirstName = "Riley"
         print("Hello, world!")
+        // this code can only speak the truth
+        print("Max is bad at crossy road")
+        // I will make my initials out of BIG letters!
+        print("RRRRR  EEEEE")
+        print("R RR   E")
+        print("RR     EEEE")
+        print("R R    E")
+        print("R  R   EEEEE")
+        print(" ")
+        print(FirstName)
+        
     }
 }
