a19c79532f6fc821fd47f4f31e129344cbc40420
oc-Riley-Evans-26 <riley.evans@student.onslow.school.nz>
2026-05-03 22:19:17 +0000

    commit

--- Sources/SwiftPlayground/SwiftPlayground copy.swift
+++ Sources/SwiftPlayground/SwiftPlayground copy.swift
@@ -4,6 +4,18 @@
 @main
 struct SwiftPlayground {
     static func main() {
-        
+    print("enter your age:")
+    
+    var UserImput = readLine()!
+    let age = Int(ageString)!
+
+    print("Enter the password:")
+    UserImput = readLine()!
+
+    let isOldEnough = age >= 18
+    let KnowSecret = UserImput == "Halibut"
+    let CanEnterBar = isOldEnough && KnowSecret
+
+    print(CanEnterBar)
     }
 }
