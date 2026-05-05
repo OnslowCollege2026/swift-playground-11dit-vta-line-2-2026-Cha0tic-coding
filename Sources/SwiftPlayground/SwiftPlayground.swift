16578038cadad271aa15b739d19b35003f12d260
oc-Riley-Evans-26 <riley.evans@student.onslow.school.nz>
2026-05-05 21:55:31 +0000

    commit

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -4,6 +4,15 @@
 @main
 struct SwiftPlayground {
     static func main() {
+var adultFares: [Int] = [800, 1200, 1500]
+//print(adultFares)
+
+var childFares: [Int] = [400, 600, 750]
+//print(childFares)
+
+var ticketTypes: [String] = ["24-hour", "48-hour", "72-hour"]
+
+print("what is your age?")
 
     }
 }
