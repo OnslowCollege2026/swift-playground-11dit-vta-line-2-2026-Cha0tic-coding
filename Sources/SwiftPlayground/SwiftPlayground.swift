e855c346a53952eba009391870bfd75987e2b213
oc-Riley-Evans-26 <riley.evans@student.onslow.school.nz>
2026-05-12 21:10:15 +0000

    commit

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -5,29 +5,5 @@
 struct SwiftPlayground {
     static func main() {
 
-        print("welcome to the number screen, press enter to begin.")
-        print(readLine())
-
-        stride(from: 1, to: 100, by: 2).forEach
-        { i in
-            print("Hello!\(i)")
-        }
-
-        //let items = ["Max", "Riley", "Richard"]
-
-        //print("Those who are invited to my birthday party")
-        //items.forEach { item in
-        //print("\(item) is invited to my birthday party")
-        //}
-
-
-        //print("Chose a number")
-        //let x = readLine()
-
-        //repeat {
-        //    var x = 1
-        //print (x)
-        //x = x * 2
-        //} while Int x <= 100
     }
 }
