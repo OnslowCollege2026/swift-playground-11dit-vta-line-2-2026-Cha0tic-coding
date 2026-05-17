80c4aba66f3deec5c788759aefdf1de5b4fbfde0
oc-Riley-Evans-26 <riley.evans@student.onslow.school.nz>
2026-05-17 22:03:30 +0000

    commit

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -12,7 +12,7 @@ var ADD2 = false
 
 while ADD1 == true {
 print("who do you want to invite?")
-var person = readLine()!
+let person = readLine()!
 
 if person == "" {
     ADD1 = false
@@ -25,17 +25,18 @@ ADD2 = true
 
 while ADD2 == true{
     print("who else do you want to invite?")
-var person2 = readLine()!
+let person2 = readLine()!
+
+
 if person2 == ""  {
     ADD2 = false
-} else if ADD2 == false {
-    ADD2 = true
-} else  {
+} 
+else  {
     invitees.append(person2)
-    ADD2 = false
 }
 
+if ADD2 == false && ADD1 == false {
 invitees.forEach { invitee in
 print("\(invitee) will be invited")
 
-}}}}}
+}}}}}}
