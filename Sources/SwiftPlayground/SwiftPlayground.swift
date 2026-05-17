55025b2564a35c517136d38efc8207813c717d36
oc-Riley-Evans-26 <riley.evans@student.onslow.school.nz>
2026-05-17 22:41:42 +0000

    commit

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -4,6 +4,24 @@
 @main
 struct SwiftPlayground {
     static func main() {
+var ingredients: [String] = []
+var isRunning = true
 
+while isRunning == true {
+    print("What ingredients do you have?")
+    var input = readLine()!.lowercased()
+if input == "chocolate"{
+    ingredients.append(input)
+    isRunning = false
+} else {
+isRunning = false
+}
+}
 
-    }}
+print(ingredients)
+if ingredients == "chocolate" {
+    print("yum")
+}
+}
+
+    }
