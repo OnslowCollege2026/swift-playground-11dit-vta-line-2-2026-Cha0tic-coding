0defb7aa211d04628701efe3daa089efdd2ae27c
oc-Riley-Evans-26 <riley.evans@student.onslow.school.nz>
2026-05-19 21:24:49 +0000

    commit

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -4,30 +4,3 @@
 @main
 struct SwiftPlayground {
     static func main() {
-var ingredients: [String] = []
-var isRunning = true
-
-while isRunning == true {
-    print("What ingredients do you have?")
-    let input = readLine()!.lowercased()
-if input == "chocolate"{
-    ingredients.append(input)
-    isRunning = false
-} 
-else {
-ingredients.append(input)
-}
-
-
-print(ingredients)
-if input == "chocolate" {
-    print("Yum!")
-    print("Let's start baking.")
-}
-
-}}}
-
-// identify input and output
-// discouver variables/constants
-// run calculations
-// draft it up, cuh
