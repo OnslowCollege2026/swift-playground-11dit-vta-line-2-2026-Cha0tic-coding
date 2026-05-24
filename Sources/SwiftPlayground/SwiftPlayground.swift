979026a1fa33cda9c0601d275952d82c1ab4dbd0
oc-Riley-Evans-26 <riley.evans@student.onslow.school.nz>
2026-05-24 22:26:10 +0000

    commit

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -4,37 +4,4 @@
 @main
 struct SwiftPlayground {
     static func main() {
-var isRunning = true
-var times: [String] = []
-
-print("Enter the length of the movie in minutes:")
-
-if var input = readLine(), var movieLength = Int(input) { 
-    if movieLength < 30 || movieLength > 300 {
-        print("Movie length out of bounds.")
-    } else if movieLength > 30 && movieLength < 300 {
-        print("Movie length accepted: \(movieLength) minutes.")
-        times.append(input)
     }
-else {
-    print("Please enter a valid number for the movie length.")
-}
-
-print("press E. to enter another movie time")
-var input2 = readLine()?.uppercased()
-
-if input2 == "Q" {
-    isRunning = true
-} else {
-    print(times)
-}
-
-while isRunning == true {
-    print("Enter another movie time:")
-if movieLength < 30 {
-
-}
-else if movieLength > 300 {
-
-}
-    }}}
