04e3dbaadd4876d5090354b8ec9cd826d136f4c3
oc-Riley-Evans-26 <riley.evans@student.onslow.school.nz>
2026-05-26 21:49:43 +0000

    commit

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -4,23 +4,10 @@
 @main
 struct SwiftPlayground {
     static func main() {
-    
-// Price of the bread is $19.99
-let Price: Double = 19.99
+        
+print("The length of the rectangle is \(12.5) units.")
+print("The width of the rectangle is \(8) units.")
+print("The area of the rectangle is \(12.5 * 8.0) square units.")
+print("The perimeter of the rectangle is \(2 * (length + width)) units.")
 
-let Quantity: Double = 3.0
-// price * quantity = subtotal
-let Subtotal: Double = 19.99 * 3.0
-// 15% tax rate
-let Tax: Double = 19.99 * 3.0 * 0.15
-// Net cost
-let Total: Double = 19.99 * 3.0 - Tax
-
-print("Price per item: $\(Price)")
-print("Quantity: \(Quantity)")
-print("Quantity x Price = \(Subtotal)")
-print("Subtotal: $\(Subtotal)")
-print("Tax (15%): $\(Tax)")
-print("Subtoal - Tax = \(Total)")
-print("Total: $\(Total)")
     }}
