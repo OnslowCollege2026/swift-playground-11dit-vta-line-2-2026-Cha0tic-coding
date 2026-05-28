e5413dcee041f2092d7e05b5855b4b691355c8d1
oc-Riley-Evans-26 <riley.evans@student.onslow.school.nz>
2026-05-28 02:20:18 +0000

    commit

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -8,6 +8,6 @@ struct SwiftPlayground {
 print("The length of the rectangle is \(12.5) units.")
 print("The width of the rectangle is \(8) units.")
 print("The area of the rectangle is \(12.5 * 8.0) square units.")
-print("The perimeter of the rectangle is \(2 * (length + width)) units.")
+print("The perimeter of the rectangle is \(2 * (length + width) units.")
 
     }}
