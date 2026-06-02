de25543c08b6e995acda66071f7f6058fc848e07
oc-Riley-Evans-26 <riley.evans@student.onslow.school.nz>
2026-06-02 21:09:18 +0000

    commit

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -5,23 +5,6 @@
 struct SwiftPlayground {
     static func main() {
     
-var isRunning = true
 
-/// Price of ticket.
-let ticketPrice: Double = 17.5
-
-/// Amount of friends.
-let numberOfFriends = 8
-
-if let isRunning == true {
-    print("The cost of each ticket is $\(ticketPrice).")
-    print("You have \(numberOfFriends) friends.")
-    print("Total price is \(numberOfFriends))
-    print("Would you like to give a tip? Y/N")
-    var userInput = readLine()?.uppercased()
-} 
-if userInput = "Y" {
-
-}
 
     }}
