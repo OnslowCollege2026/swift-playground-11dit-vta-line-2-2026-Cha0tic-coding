ca5cde382fb978800893a9f4ff292f6b6730cca7
oc-Riley-Evans-26 <riley.evans@student.onslow.school.nz>
2026-06-03 02:51:46 +0000

    Jun 3, 2026, 2:51 AM

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -4,16 +4,8 @@
 @main
 struct SwiftPlayground {
     static func main() {
-var x = 2
 
-
-
-while x < 2305843009213693952  {
-    x = x + 1
-    print(x)
-} else x < 27082 {
-    x = 2
-}
+/// Create a 
 
 
 
