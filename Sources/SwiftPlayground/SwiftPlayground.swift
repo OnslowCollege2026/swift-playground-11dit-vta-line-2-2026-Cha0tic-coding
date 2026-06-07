5d543fb2c4d12b402324bde06c05692480f6911d
oc-Riley-Evans-26 <riley.evans@student.onslow.school.nz>
2026-06-07 22:50:54 +0000

    commit

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -5,20 +5,49 @@
 struct SwiftPlayground {
     static func main() {
 
-/// Create a aisle for a mall
+var isRunning = true
 
-let isRunning = true
+while isRunning == true {
+print("Welcome to Zealandia tracker")
+print(" ")
+print("Did you see a bird or a insect?")
 
-func wareHouse() {
-    print("A. to enter the Warehouse.")
-    print("B. to enter the Supermarket.")
-    print("C. to do a backflip")
-    print("D. ")
-    print("Q. to leave the console")
+/// typing here whether they saw bird or insect
+var birdOrInsect = readLine()?.lowercased()
+
+/// if they typed "bird"
+if birdOrInsect == "bird" {
+    print("1. Tieke")
+print("2. Kākā")
+print("3. Takahē")
+print("4. Hihi")
+print("5. Kiwi")
+print("6. Pāteke")
+print("7. Tūī")
+print("8. Kererū")
+print("Enter a number: 8")
+
+
+} 
+/// if they typed "insect"
+else if birdOrInsect == "insect" {
+print("1. Giant Wētā")
+print("2. Tree Wētā")
+print("3. Cave Wētā")
+print("4. Putoko")
+print("5. Pūngāwerewere")
+print("6. Ngaokeoke")
+print("7. Waemano")
+print("8. Kapowai")
+print("Enter a number:")
+
+} 
+/// if they did not type either
+else {
+    print("This is not a valid response")
+    isRunning = false
 }
 
-
-
-    }
-
 }
+
+    }}
