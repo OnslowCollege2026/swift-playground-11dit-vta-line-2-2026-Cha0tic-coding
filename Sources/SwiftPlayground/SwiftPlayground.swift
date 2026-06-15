888fdd8f5a05731aca9072a70d7ec450affcff19
oc-Riley-Evans-26 <riley.evans@student.onslow.school.nz>
2026-06-15 07:03:11 +0000

    commit

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -6,6 +6,33 @@ struct SwiftPlayground {
     static func main() {
 
 
+let sleepHours: [String] = []
+var isRunning = true
+//var fellAsleepTime: Int = 0
+//var wakeUpTime: Int = 0
+
+
+
+print("")
+print("Hello, welcome to the 'SleepHour' terminal")
+print("")
+
+if isRunning == true {
+    print("What time did you fall asleep on Monday?")
+    print("Enter a number:")
+
+    /// this makes sure that the answer is a number
+var fellAsleepTime = Int (readLine()!)!
+
+    print("What time did you wake up on Tuesday?")
+    print("Enter a number:")
+var wakeUpTime = Int (readLine()!)!
+
+var nightSleepTime: Int = 12 - fellAsleepTime
+var netSleepTime: Int = nightSleepTime + wakeUpTime
+    print("You slep \ne")
+    print("Added \(netSleepTime) hours.")
+}
 
 
     }}
