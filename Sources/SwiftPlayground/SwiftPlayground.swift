1d1f7133343990ba805131d439db3f2924c55e95
oc-Riley-Evans-26 <riley.evans@student.onslow.school.nz>
2026-06-16 21:57:31 +0000

    commit

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -5,34 +5,94 @@
 struct SwiftPlayground {
     static func main() {
 
-
-let sleepHours: [String] = []
+/// Enables the while loop
 var isRunning = true
-//var fellAsleepTime: Int = 0
-//var wakeUpTime: Int = 0
 
+/// This will record the total ammount of hours.
+var totalHours: Int = 0
 
+/// amount of days in a week.
+let totalDays: Int = 5
+
+/// time untill midnight will be used in the equation.
+let timeUntillMidnight = 12
+
+/// This is the equation to find the average amount of hours.
+var averageHours: Int = totalHours / totalDays
+
+/// These variables change the day of the week
+var day: Int = 0
+var nextDay: Int = 1
+
+/// These days of the week will be used for the while loop.
+let fallAsleepDays: [String] = ["Monday", "Tuesday", "Wednesday", "Thursday"]
+let wakeUpDays: [String] = ["Tuesday", "Wednesday", "Thursday", "Friday"]
 
 print("")
 print("Hello, welcome to the 'SleepHour' terminal")
 print("")
 
-if isRunning == true {
-    print("What time did you fall asleep on Monday?")
+while isRunning == true {
+
+    print("What time did you fall asleep on \(fallAsleepDays[day])?")
+
     print("Enter a number:")
 
-    /// this makes sure that the answer is a number
+/// this makes sure that the answer is a number
 var fellAsleepTime = Int (readLine()!)!
 
-    print("What time did you wake up on Tuesday?")
+    print("What time did you wake up on \(wakeUpDays[day])?")
     print("Enter a number:")
+
 var wakeUpTime = Int (readLine()!)!
 
-var nightSleepTime: Int = 12 - fellAsleepTime
+
+
+/// This is the equation to find the total sleep time of the night
+var nightSleepTime: Int = timeUntillMidnight - fellAsleepTime
 var netSleepTime: Int = nightSleepTime + wakeUpTime
-    print("You slep \")
-    print("Added \(netSleepTime) hours.")
+
+print("you slept for \(netSleepTime) hours.")
+print("Added \(netSleepTime) hours to the total.")
+print("")
+
+
+/// Adds the netSleepTime to the total amount of hours they slept
+totalHours += netSleepTime
+
+/// increases the while loop
+day += nextDay
+
+/// once the week ends, finish the while loop
+if day > 4 {
+    isRunning = false
 }
 
+}
 
-    }}
+print("")
+print("You slept a total of \(totalHours) hours.")
+print("")
+print("Through the week you slept an average of \(averageHours) hours every night")
+print("")
+
+if averageHours >= 8 {
+    print("Congrats, You slept a healthy amount of hours this week")
+}
+else if averageHours < 8 {
+    print("You need to sleep more bucko")
+}
+
+print("")
+print("Do you want to record another week? (Y/N)")
+
+let anotherWeek = readLine()?.lowercased()
+
+if anotherWeek == "y" {
+    let isRunning = true
+} else if anotherWeek == "n" {
+    print("alr, bye bye")
+} else {
+    print("This is not a valid response.")
+}
+}}
